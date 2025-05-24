65a83b121156138ec61c50b881ad7b05e02131f4
Selly Beyene <sellybeyene@outlook.com>
2025-05-23 20:16:01 -0700

    Added the values/culture to landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,13 @@ import CardActions from '@mui/material/CardActions';
 const page = () => {
   return (
     <div>
-      <div style={{ display: 'flex', alignItems: 'center', gap: '7rem'}}>
-        <div>
-          <Image src="/images/logo.png" alt="Logo" width={500} height={120} />
-          <h1>Illuminance</h1>
-          <h1>Esthetics</h1>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '7rem', justifyContent: 'space-between', padding: '3rem'}}>
+        <div style={{ width: '100%', maxWidth: '400px', height: 'auto'}}>
+          <Image src="/images/logo.png" alt="Logo" width={500} height={120} style={{ width: '100%', height: 'auto'}}/>
+          <h1 className='title'>Illuminance</h1>
+          <h1 >Esthetics</h1>
         </div>
-        <div style={{ width: 600, height: 200 }}>
+        <div style={{ position: 'relative', width: '40%', minWidth: 250, height: 200 }}>
           <VideoPlayer videoId='R3v_u-vUbsU'/>
         </div>
 
@@ -33,15 +33,15 @@ const page = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
         </p>
       </div>
-      <div style={{ paddingLeft: '4rem', paddingTop: '4rem', display: 'flex', alignItems: 'center', gap: '3rem'}}>
-        <Card sx={{ maxWidth: 345 }}>
+      <div style={{ padding: '3rem', display: 'flex', flexWrap: 'wrap', gap: '5rem', justifyContent: 'center' }}>
+        <Card sx={{ maxWidth: '70%', minWidth: 250, flex: '1 1 300px', backgroundColor: '#307346'}}>
           <CardActionArea>
             <CardMedia component="img" height="140" image="/images/tree.png" alt="tree"/>
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
+              <Typography gutterBottom variant="h5" component="div" sx={{ color: '#fff' }}>
                 Esthetics & Eyelashes
               </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary'}}>
+              <Typography variant="body2" sx={{ color: '#fff' }}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
               </Typography>
             </CardContent>
@@ -53,14 +53,14 @@ const page = () => {
           </CardActions>
         </Card>
 
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: '70%', minWidth: 250, flex: '1 1 300px', backgroundColor: '#307346' }}>
           <CardActionArea>
             <CardMedia component="img" height="140" image="/images/tree.png" alt="tree"/>
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
+              <Typography gutterBottom variant="h5" component="div" sx={{ color: '#fff' }}>
                 Esthetics & Eyelashes
               </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary'}}>
+              <Typography variant="body2" sx={{ color: '#fff'}}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
               </Typography>
             </CardContent>
@@ -72,14 +72,14 @@ const page = () => {
           </CardActions>
         </Card>
 
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: '70%', minWidth: 250, flex: '1 1 300px', backgroundColor: '#307346' }}>
           <CardActionArea>
             <CardMedia component="img" height="140" image="/images/tree.png" alt="tree"/>
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
+              <Typography gutterBottom variant="h5" component="div" sx={{ color: '#fff' }}>
                 Esthetics & Eyelashes
               </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary'}}>
+              <Typography variant="body2" sx={{ color: '#fff'}}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
               </Typography>
             </CardContent>
@@ -90,6 +90,19 @@ const page = () => {
             </Button>
           </CardActions>
         </Card>
+      </div>
+      <h1 style={{paddingTop: '4rem'}}>Our Values & Culture</h1>
+      <div style={{ backgroundColor: '#36804F', padding: '4rem', color: '#fff'}}>
+        <h2 >Growth</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+      <div style={{ backgroundColor: '#307346', padding: '4rem', textAlign: 'right', paddingLeft: '4rem', paddingRight: '4rem', color: '#fff'}}>
+        <h2 >Quality</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+      <div style={{ backgroundColor: '#97B082', padding: '4rem', paddingLeft: '4rem', paddingRight: '4rem', color: '#fff'}}>
+        <h2>Education</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
       </div>
     </div>
 
